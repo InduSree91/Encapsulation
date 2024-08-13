a924340a93fd73b3cfdc0bbe08f471870b6d075c
Indu Sree <Indu Sree@DESKTOP-UN8GH21>
2024-08-13 15:54:41 +0530

    Add project files.

---
+++ Encapsulation/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Encapsulation
+{
+    class NewClass
+    {
+        private string StudentName;
+        private int StudentAge;
+
+        public string Name
+        {
+            get { return StudentName; }
+            set { StudentName = value; }
+        }
+
+        public int Age
+        {
+            get { return StudentAge; }
+            set { StudentAge = value; }
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NewClass obj1 = new NewClass();
+
+            obj1.Name = "Sai";
+            obj1.Age = 27;
+
+            Console.WriteLine("Name : " + obj1.Name);
+            Console.WriteLine("Age : " + obj1.Age);
+        }
+    }
+}
